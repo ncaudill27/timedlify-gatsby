71e6e20e56ddd505dccd6908bd82096212b7d6a7
Nelson Caudill <nelson.a.caudill@gmail.com>
2021-12-12 01:10:27 -0500

    convert rgb background grabbed from ref to hsl

--- src/hooks/useGlassmorphism.js
+++ src/hooks/useGlassmorphism.js
@@ -1,12 +1,15 @@
 import { useState, useEffect } from "react"
+import { rgbToHsl } from "../utils/convertingColor"
 
 const useGlassmorphism = (ref, options) => {
   const [background, setBackground] = useState("")
 
   useEffect(() => {
     console.log("render")
-    if (ref.current) {
-      console.log(getComputedStyle(ref.current))
+    if (typeof window !== undefined && ref.current) {
+      const rgb = getComputedStyle(ref.current).backgroundColor
+      const hsl = rgbToHsl(rgb)
+      console.log(hsl)
     }
   }, [ref])
   return "hello"
